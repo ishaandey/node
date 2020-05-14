47d3f1c4a71f9a1d1685f4f1508940af5c147c13
ishaandey <ishaan.dey8@gmail.com>
2020-05-14 13:54:18 -0400

    Finish convert script i/o

--- tools/convertKey.py
+++ tools/convertKey.py
@@ -4,21 +4,21 @@ import io, os, sys
 fpath = input('Input path to .ipynb answer key: ')
 outdir = input('Output directory: ')
 fname = fpath.split('/')[-1].split('.')[0]
-outpath = outdir+'/'+fname+'_notes'+'.ipynb'
+outpath = outdir+'/'+fname.replace('_key','_notes')+'.ipynb'
 print(outpath)
 
 def main():
     with open(fpath) as f:  
         nb_key = json.load(f)
-    print(nb_key)
 
     nb_notes = nb_key 
 
-    if 'answer' in str(nb_notes['cells'][1]['metadata']):
-        del nb_notes['cells'][1]
+    for index, cell in enumerate(nb_notes['cells']):
+        if 'answer' in str(cell['metadata']):
+            nb_notes['cells'][index]['source'] = ""
 
-    with open(outpath, 'w') as f:
-        f.write(str(nb_key))
+    with open(outpath, 'w') as outfile:
+        json.dump(nb_notes, outfile)
 
 if __name__ == "__main__":
     main()
