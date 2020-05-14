9816b39b275d6e2ed91db121952bf5c22c0fa279
ishaandey <ishaan.dey8@gmail.com>
2020-05-14 14:08:50 -0400

    Clean up

--- tools/convertKey.py
+++ tools/convertKey.py
@@ -22,27 +22,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-# @click.group()
-# def main():
-#     """
-#     Simple CLI for converting answer keys into workshop-ready interactive Python notebooks
-#     """
-#     pass
-
-# @main.command()
-# @click.argument('fpath', help='File path of answer key')
-# # @click.argument('--outdir', help='Directory to output notebook')
-
-# def create(fpath):
-#     """
-#     Remove cells w/ metadata marked as key
-#     """
-#     with open(fpath, 'r') as f:  
-#         nb = json.load(fpath)
-#     print(nb)
-#     click.echo(nb)
-
-# if __name__ == "__main__":
-#     main()
