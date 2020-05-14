4e1594a524a6dfe9faeb7b9ee8219b257782b871
ishaandey <ishaan.dey8@gmail.com>
2020-05-14 13:37:12 -0400

    Add basic nb i/o for convert key

---
+++ tools/convertKey.py
@@ -0,0 +1,48 @@
+import json 
+import io, os, sys
+
+fpath = input('Input path to .ipynb answer key: ')
+outdir = input('Output directory: ')
+fname = fpath.split('/')[-1].split('.')[0]
+outpath = outdir+'/'+fname+'_notes'+'.ipynb'
+print(outpath)
+
+def main():
+    with open(fpath) as f:  
+        nb_key = json.load(f)
+    print(nb_key)
+
+    nb_notes = nb_key 
+
+    if 'answer' in str(nb_notes['cells'][1]['metadata']):
+        del nb_notes['cells'][1]
+
+    with open(outpath, 'w') as f:
+        f.write(str(nb_key))
+
+if __name__ == "__main__":
+    main()
+
+
+# @click.group()
+# def main():
+#     """
+#     Simple CLI for converting answer keys into workshop-ready interactive Python notebooks
+#     """
+#     pass
+
+# @main.command()
+# @click.argument('fpath', help='File path of answer key')
+# # @click.argument('--outdir', help='Directory to output notebook')
+
+# def create(fpath):
+#     """
+#     Remove cells w/ metadata marked as key
+#     """
+#     with open(fpath, 'r') as f:  
+#         nb = json.load(fpath)
+#     print(nb)
+#     click.echo(nb)
+
+# if __name__ == "__main__":
+#     main()
