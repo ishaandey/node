ecda5a08f3c3ca8b70e2439c60a83a40aa1ff165
ishaandey <ishaan.dey8@gmail.com>
2020-05-15 11:21:37 -0400

    Allow flexible tag removal

--- tools/convertKey.py
+++ tools/convertKey.py
@@ -3,6 +3,8 @@ import io, os, sys
 
 fpath = input('Input path to .ipynb answer key: ')
 outdir = input('Output directory: ')
+tags = input('Tags to remove (seperate by space): ').lower()
+
 fname = fpath.split('/')[-1].split('.')[0]
 outpath = outdir+'/'+fname.replace('_key','_notes')+'.ipynb'
 print(outpath)
@@ -14,8 +16,9 @@ def main():
     nb_notes = nb_key 
 
     for index, cell in enumerate(nb_notes['cells']):
-        if 'answer' in str(cell['metadata']):
-            nb_notes['cells'][index]['source'] = ""
+        for tag in tags.split():
+            if tag in str(cell['metadata']):
+                nb_notes['cells'][index]['source'] = ""
 
     with open(outpath, 'w') as outfile:
         json.dump(nb_notes, outfile)
